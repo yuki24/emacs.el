51ae0485a3c5d26402925320a88c1130319b2b8d
Yuki Nishijima <mail@yukinishijima.net>
2012-09-08 21:44:01 +0900

    Enable syntax-highlighting for rakefiles and others

--- .emacs.el
+++ .emacs.el
@@ -4,6 +4,13 @@
 ;; Rinari
  (add-to-list 'load-path "/path/to/.emacs.d/rinari")
  (require 'rinari)
+;; Rake files are ruby, too, as are gemspecs, rackup files, etc.
+ (add-to-list 'auto-mode-alist '("\.rake$" . ruby-mode))
+ (add-to-list 'auto-mode-alist '("\.gemspec$" . ruby-mode))
+ (add-to-list 'auto-mode-alist '("\.ru$" . ruby-mode))
+ (add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))(add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
+ (add-to-list 'auto-mode-alist '("Capfile$" . ruby-mode))
+ (add-to-list 'auto-mode-alist '("Vagrantfile$" . ruby-mode))
 
 ;;日本語環境
 (set-language-environment 'Japanese)
